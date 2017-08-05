285367d5333bbf4ff645f15c0f2e02e4466cf4af
Matthew Shang <mshang47@gmail.com>
2017-08-05 15:09:23 -0700

    Removed extra code

--- src/chunk.h
+++ src/chunk.h
@@ -22,9 +22,7 @@ public:
     Chunk(glm::ivec3 pos);
     ~Chunk();
 
-    //void buildMesh();
     void bufferData();
-    //void calcLighting(ChunkMap &chunks);
 
     void compute(ChunkMap &chunks);
 
@@ -45,28 +43,6 @@ public:
 
 private:
     void initBlocks();
-
-    struct ChunkData
-    {
-        uint8_t lightMap[3 * CHUNK_SIZE][3 * CHUNK_SIZE][3 * CHUNK_SIZE];
-        bool opaqueMap[48][48][48];
-    };
-
-    struct LightNode
-    {
-        LightNode(int _x, int _y, int _z, int _light)
-            : x(_x), y(_y), z(_z), light(_light) {};
-
-        int x, y, z;
-        uint8_t light;
-    };
-
-    void getLights(glm::ivec3 &delta, std::queue<LightNode> &queue, ChunkData &blocks);
-    void calcLighting(ChunkMap &chunks, ChunkData &data);
-    void smoothLighting(int x, int y, int z, ChunkData &data, float light[6][4]);
-    void smoothLighting2(int x, int y, int z, ChunkData &data, float light[6][4]);
-    void faceLighting(int x, int y, int z, ChunkData &data, float light[6][4]);
-    void buildMesh(ChunkData &data);
 
     GLuint m_vao;
     GLuint m_vbo;
